36cbc03e91613b571f33f97e49544556cbd923e9
Joao Moreira <joao.moreira@distilled.ie>
2018-09-13 18:57:01 +0100

    Java setup with junit

---
+++ java/src/main/java/ie/distilledsch/codekata/Main.java
@@ -0,0 +1,8 @@
+package ie.distilledsch.codekata;
+
+public class Main {
+    public static void main(String[] args) {
+        Song song = new Song();
+        System.out.println(song.sing());
+    }
+}
---
+++ java/src/test/java/ie/distilledsch/codekata/SongTest.java
@@ -0,0 +1,12 @@
+package ie.distilledsch.codekata;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SongTest {
+
+    @Test
+    public void testSong() {
+        Assert.assertTrue("stupid test", false);
+    }
+}
