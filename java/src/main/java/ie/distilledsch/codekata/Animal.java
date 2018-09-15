c2f611ac9c18be312294ae68d2dbacfce482e0c9
marksills <mark@donedeal.ie>
2018-09-15 09:11:22 +0100

    v1

---
+++ java/src/main/java/ie/distilledsch/codekata/Animal.java
@@ -0,0 +1,12 @@
+package ie.distilledsch.codekata;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Animal {
+
+    private String name;
+    private String line;
+}
